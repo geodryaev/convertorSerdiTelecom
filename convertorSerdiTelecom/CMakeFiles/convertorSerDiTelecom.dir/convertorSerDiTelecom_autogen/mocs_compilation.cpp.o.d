58d1f3602e77803d93d99d643cece19acf39fadf
DjJohnyBoy <geodryaev1@gmail.com>
2025-06-27 12:08:17 +0300

    delete dir

--- convertorSerdiTelecom/CMakeFiles/convertorSerDiTelecom.dir/convertorSerDiTelecom_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/convertorSerDiTelecom.dir/convertorSerDiTelecom_autogen/mocs_compilation.cpp.o: \
- /home/user/build-convertorSerDiTelecom-Desktop-Debug/convertorSerDiTelecom_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
